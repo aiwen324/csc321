80efb630400c06062d4cd84a3645a567177385ea
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:33:15 +0000

    Adding numeric_owner as keyword arguement

--- assignment2/load_data.py
+++ assignment2/load_data.py
@@ -61,7 +61,7 @@ def get_file(fname,
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(archive, datadir)
